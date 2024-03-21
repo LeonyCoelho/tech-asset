610b4004b7842f47734971108504e7f1fdfdf404
LeonyCoelho <leony.coelho@outlook.com>
2024-03-21 01:42:26 -0300

    admin.py_done

--- src/tech_asset/app_tech_asset/admin.py
+++ src/tech_asset/app_tech_asset/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import CustomUser, GlobalSettings, Category, SubCategory, Sector, RentalCompany, Asset, Kit, AssetQR, AssetHistory
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['full_name']
+
+@admin.register(GlobalSettings)
+class GlobalSettingsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Sector)
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(RentalCompany)
+class RentalCompanyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'sector', 'rental_company']
+
+@admin.register(Kit)
+class KitAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+@admin.register(AssetQR)
+class AssetQRAdmin(admin.ModelAdmin):
+    list_display = ['asset', 'qr_code']
+
+@admin.register(AssetHistory)
+class AssetHistoryAdmin(admin.ModelAdmin):
+    list_display = ['asset_id', 'modification_type', 'name', 'category', 'modified', 'user', 'sector', 'rental_company', 'internal_code1', 'internal_code2', 'internal_code3', 'serial_number', 'brand', 'mac_address', 'windows_license']
